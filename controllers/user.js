1b7214a66c6a8edd899b9758acb700864ab1fa88
Rene Santiago <rsj510@gmail.com>
2022-12-03 12:35:26 -0500

    added user controller

--- controllers/user.js
+++ controllers/user.js
@@ -15,5 +15,23 @@ const router = express.Router()
 /////////////////////////////////////////
 // The Signup Routes (Get => form, post => submit form)
 router.get("/signup", (req, res) => {
-    
+    res.render("user/signup.ejs")
 })
+
+router.post("/signup", (req, res) => {
+    res.send("signup")
+})
+
+// The login Routes (Get => form, post => submit form)
+router.get("/login", (req, res) => {
+    res.render("user/login.ejs")
+})
+
+router.post("/login", (req, res) => {
+    res.send("login")
+})
+
+//////////////////////////////////////////
+// Export the Router
+//////////////////////////////////////////
+module.exports = router
